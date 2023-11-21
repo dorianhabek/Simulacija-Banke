1614895f66ade0a586a04aca272746b10c06e77a
dorianhabek <143107236+dorianhabek@users.noreply.github.com>
2023-11-21 17:44:09 +0100

    Update Sekvence.sql
    Napravljene sekvence za sve primary key-eve te za neke foreign key-eve radi lakšeg daljnjeg unosa u bazu

--- Sekvence/Sekvence.sql
+++ Sekvence/Sekvence.sql
@@ -2,3 +2,53 @@ create sequence serijski_broj
 minvalue 1
 start with 1
 increment by 1;
+
+create sequence pk_id_klij
+minvalue 1
+start with 1
+increment by 1;
+
+create sequence fk_id_klij_det
+minvalue 1
+start with 1
+increment by 1;
+
+create sequence pk_id_klij_det
+minvalue 1
+start with 1
+increment by 1;
+
+create sequence pk_id_poslovnice
+minvalue 10
+start with 10
+increment by 10;
+
+create sequence pk_id_zaposlenika
+minvalue 10
+start with 10
+increment by 10;
+
+create sequence pk_id_det_zaposlenika
+minvalue 101
+start with 101
+increment by 1;
+
+create sequence pk_id_zaposlenika_det
+minvalue 10
+start with 10
+increment by 10;
+
+create sequence pk_id_obracun_place
+minvalue 1000
+start with 1000
+increment by 1;
+
+create sequence pk_broj_racuna
+minvalue 1001
+start with 1001
+increment by 1;
+
+create sequence pk_id_kredita
+minvalue 101
+start with 101
+increment by 1;
