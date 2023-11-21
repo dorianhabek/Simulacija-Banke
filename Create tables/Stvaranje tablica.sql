a915c9d9b813d3389d59a572778d190dc8410028
dorianhabek <143107236+dorianhabek@users.noreply.github.com>
2023-11-21 14:24:27 +0100

    Update Stvaranje tablica.sql
    Novi create table file s foreign key constraintovima

--- Create tables/Stvaranje tablica.sql
+++ Create tables/Stvaranje tablica.sql
@@ -1,23 +1,26 @@
 create table klijenti(
     id_klijenta number primary key not null ,
     ime_klijenta varchar2(50) not null,
-    prezime_klijenta varchar2(50) not null,
-    id_detalji_klijenta number not null unique,
-    broj_racuna number not null unique
+    prezime_klijenta varchar2(50) not null
 );
-
 
 create table detalji_klijenta(
     id_detalji_klijenta  number primary key not null ,
+    id_klijenta number,
     email_adresa varchar2(50),
     telefon number not null,
     adresa varchar2(50) not null,
     grad varchar2(50) not null,
-    post_broj number not null 
+    post_broj number not null ,
+    constraint fk_id_klijenta_det
+    foreign key (id_klijenta)
+    references klijenti(id_klijenta)
 );
 
 create table racuni(
-    broj_racuna number primary key not null ,
+    broj_racuna number primary key not null,
+    id_klijenta number,
+    id_zaposlenika number,
     balans number not null,
     autentifikacija varchar2(50) not null unique,
     dnevni_limit number not null,
@@ -26,23 +29,37 @@ create table racuni(
     tip_racuna varchar2(50) not null,
     valuta varchar2(50) not null,
     datum_stvaranja date not null,
-    id_zaposlenika number not null  
+    constraint fk_id_zaposlenika_rac
+    foreign key(id_zaposlenika)
+    references zaposlenici_poslovnice(id_zaposlenika),
+    
+    constraint fk_id_klijenta_rac
+    foreign key(id_klijenta)
+    references klijenti(id_klijenta)
 );
-
-
 
 create table krediti(
     id_kredita number primary key not null ,
     id_klijenta number,
-    broj_racuna number not null unique,
+    broj_racuna number not null,
     tip_kredita varchar2(50) not null,
     iznos_kredita number not null,
     kamata number not null,
     datum_izdavanja date not null,
     datum_završetka date not null,
-    id_zaposlenika number not null unique  
+    id_zaposlenika number not null,
+    constraint fk_id_zaposlenika_kred
+    foreign key(id_zaposlenika)
+    references zaposlenici_poslovnice(id_zaposlenika),
+    
+    constraint fk_id_klijenta_kred
+    foreign key(id_klijenta)
+    references klijenti(id_klijenta),
+    
+    constraint fk_broj_racuna_kred
+    foreign key (broj_racuna)
+    references racuni(broj_racuna)   
 );
-
 
 create table detalji_poslovnice(
     id_poslovnice number primary key not null ,
@@ -55,11 +72,15 @@ create table zaposlenici_poslovnice(
     id_zaposlenika number primary key not null ,
     ime_zaposlenika varchar2(50) not null,
     prezime_zaposlenika varchar2(50) not null,
-    id_detalji_zaposlenika number not null 
+    id_poslovnice number not null,
+    constraint fk_id_poslovnice_zap
+    foreign key(id_poslovnice)
+    references detalji_poslovnice(id_poslovnice)
 );
 
 create table detalji_zaposlenika(
     id_detalji_zaposlenika number primary key not null ,
+    id_zaposlenika number,
     email_adresa varchar2(50),
     telefon number not null,
     adresa varchar2(50) not null,
@@ -68,25 +89,30 @@ create table detalji_zaposlenika(
     titula varchar2(50),
     status varchar2(50) not null,
     dat_zaposlenja date not null,
-    id_obracun_place number not null unique,
-    id_odjela number not null  
+    
+    constraint fk_id_zaposlenika_det
+    foreign key(id_zaposlenika)
+    references zaposlenici_poslovnice(id_zaposlenika)
 );
 
-
-
 create table detalji_odjela(
-    id_odjela number primary key not null ,
+    id_odjela number not null ,
     ime_odjela varchar2(50),
-    id_zaposlenika number not null ,
-    id_poslovnice number not null 
+    id_zaposlenika number not null,
+    constraint fk_id_zaposlenika_odj
+    foreign key(id_zaposlenika)
+    references zaposlenici_poslovnice(id_zaposlenika)
 );
 
 
 create table obracun_place(
     id_obracun_place number primary key not null,
-    id_detalji_zaposlenika number not null unique,
+    id_zaposlenika number not null,
     bruto_placa number,
-    neto_placa number
+    neto_placa number,
+    constraint fk_id_zaposlenika_obr
+    foreign key(id_zaposlenika)
+    references zaposlenici_poslovnice(id_zaposlenika)
 );
 
 create table razred_place (
