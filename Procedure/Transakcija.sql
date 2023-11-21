90bd542ffa2aeb0b330e7af99ccc501f3989506c
dorianhabek <143107236+dorianhabek@users.noreply.github.com>
2023-11-21 18:19:28 +0100

    Update Transakcija.sql
    Maknut cursor za dohvat serijskog broja

--- Procedure/Transakcija.sql
+++ Procedure/Transakcija.sql
@@ -4,80 +4,71 @@ create or replace PROCEDURE TRANSAKCIJA(fr_acc number, to_acc number, doznaka va
 
     cursor cur_to_acc(to_acc number) is select broj_racuna,balans from racuni where broj_racuna = to_acc;
 
-    cursor cur_trans is select serijski_broj from transakcije;
-    
     fr_rac_row cur_fr_acc%rowtype;
     to_rac_row cur_to_acc%rowtype;
-    trans_row cur_trans%rowtype;
     kod_trans transakcije.kod_izvršenja%type;
     next_id number;
     not_found boolean;
-    
-begin
 
-    open cur_trans;
-    fetch cur_trans into trans_row;
-    not_found := cur_trans%notfound;
-    close cur_trans;
-    if not_found then
-        next_id := 1; --u slučaju novonastale tablice
-    else
-        next_id := serijski_broj.nextval;
-        kod_trans := 0; --oznaka 0 označava uspješnu transakciju
-        if doznaka = 'S' then --skidanje s računa
+begin
+    next_id := serijski_broj.nextval;
+    kod_trans := 0; --oznaka 0 označava uspješnu transakciju
+    if doznaka = 'S' then --skidanje s računa
+        open cur_fr_acc(fr_acc);
+        fetch cur_fr_acc into fr_rac_row;
+        not_found := cur_fr_acc%notfound;
+        close cur_fr_acc;
+
+        if not_found then
+            kod_trans := 2; --ne postoji račun
+        else
+            if fr_rac_row.balans < iznos then
+                kod_trans := 1; --nedovoljan saldo
+            else
+                update racuni set balans = fr_rac_row.balans - iznos
+                where broj_racuna = fr_acc;
+                commit;
+
+            end if;
+            insert into transakcije values(next_id,fr_acc,to_acc,iznos,sysdate,doznaka,kod_trans);
+            
+        end if;
+
+    elsif doznaka = 'U' then --uplata
+        if fr_acc = fr_rac_row.broj_racuna then
+                    kod_trans := 3; --nije moguče izvršiti radnju na vlastiti račun
+        else
             open cur_fr_acc(fr_acc);
             fetch cur_fr_acc into fr_rac_row;
             not_found := cur_fr_acc%notfound;
             close cur_fr_acc;
-            
             if not_found then
-                kod_trans := 2; --ne postoji račun
+                kod_trans := 2;
             else
                 if fr_rac_row.balans < iznos then
-                    kod_trans := 1; --nedovoljan saldo
+                kod_trans := 1;
+
                 else
                     update racuni set balans = fr_rac_row.balans - iznos
                     where broj_racuna = fr_acc;
-                    commit;
-                    
-                end if;
-                insert into transakcije values(next_id,fr_acc,to_acc,iznos,sysdate,doznaka,kod_trans);
-            end if;
-            
-        elsif doznaka = 'U' then --uplata
-            if fr_acc = fr_rac_row.broj_racuna then
-                        kod_trans := 3; --nije moguče izvršiti radnju na vlastiti račun
-            else
-                open cur_fr_acc(fr_acc);
-                fetch cur_fr_acc into fr_rac_row;
-                not_found := cur_fr_acc%notfound;
-                close cur_fr_acc;
-                if not_found then
-                    kod_trans := 2;
-                else
-                    if fr_rac_row.balans < iznos then
-                    kod_trans := 1;
-                
+
+                    open cur_to_acc(to_acc);
+                    fetch cur_to_acc into to_rac_row;
+                    not_found := cur_to_acc%notfound;
+                    close cur_to_acc;
+                    if not_found then
+                        kod_trans := 2;
                     else
-                        update racuni set balans = fr_rac_row.balans - iznos
-                        where broj_racuna = fr_acc;
-                        
-                        open cur_to_acc(to_acc);
-                        fetch cur_to_acc into to_rac_row;
-                        not_found := cur_to_acc%notfound;
-                        close cur_to_acc;
-                        if not_found then
-                            kod_trans := 2;
-                        else
-                            update racuni set balans = to_rac_row.balans + iznos
-                            where broj_racuna = to_acc;
-                            commit;
-                        
-                        end if;
+                        update racuni set balans = to_rac_row.balans + iznos
+                        where broj_racuna = to_acc;
+                        commit;
+
                     end if;
                 end if;
             end if;
-            insert into transakcije values(next_id,fr_acc,to_acc,iznos,sysdate,doznaka,kod_trans); 
         end if;
+        insert into transakcije values(next_id,fr_acc,to_acc,iznos,sysdate,doznaka,kod_trans);
+        
     end if;
+    
 END TRANSAKCIJA;
